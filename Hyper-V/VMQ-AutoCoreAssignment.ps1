9703d9c73f2f7c43166e709c36cbc8a823d2e10f
joris.decombe <joris.decombe@jaguar-network.com>
2017-11-16 17:52:29 +0100

    VMQ Auto Core assignment

---
+++ Hyper-V/VMQ-AutoCoreAssignment.ps1
@@ -0,0 +1,13 @@
+﻿# Enable VMQ on physical net adapters and automatically assign core over different network card depending on total number of cores
+
+# Change if needed
+$PhysicalAdapters = Get-NetAdapter -Physical # | Where Name -like "vSwitchNIC-*"
+
+[int]$NumberOfCoresPerCPU * [int]$NumberOfCPU / $PhysicalAdapters.Count
+
+$NumberOfCPU = ((Get-WmiObject –class Win32_processor).NumberOfCores).Count
+$NumberOfCoresPerCPU = (Get-WmiObject –class Win32_processor | Select -First 1).NumberOfCores
+$CorePerAdapter = $NumberOfCoresPerCPU * $NumberOfCPU / $PhysicalAdapters.Count
+$ThisBase = 1
+        
+$PhysicalAdapters | %{ Set-NetadapterVmq -BaseProcessorNumber $ThisBase -MaxProcessors $CorePerAdapter -Enabled $true ; $ThisBase = $ThisBase + $CorePerAdapter }
