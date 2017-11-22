3844e2fc33a79802c2431280b81f5d3ac8e06034
Joris DECOMBE <joris.decombe@jaguar-network.com>
2017-11-22 16:09:17 +0100

    OpenStack Windows Image Creation

---
+++ Openstack/Image/02.OpenStack-Customize-Vhdx.ps1
@@ -0,0 +1,70 @@
+# 
+# Openstack Windows Image Creation
+#
+#   . Inject qemu, cloudinit agent and unattend.xml file for first boot install
+# 
+# Auth : Joris DECOMBE
+
+$Images = Get-ChildItem "C:\Images\OpenStack\" -Filter "*.vhdx"
+
+$ToInjectPath = ".\ToInject"
+
+$InstallerPath = "\Windows\Temp"
+$UnattendPath = "\Windows\Panther\unattend.xml"
+
+ForEach ($imgfile In $Images) {
+    $namearray = ($imgfile.Name -replace ".vhdx","") -split "-"
+
+    Switch ($namearray[1]) {
+        "en" {$UnattendSource = "$ToInjectPath\Unattend-en_fr.xml"}
+        "fr" {$UnattendSource = "$ToInjectPath\Unattend-fr_fr.xml"}
+    }
+
+    Switch ($namearray[0]) {
+        "2012r2" {
+            Switch ($namearray[2]) {
+                "std"   { $key = "" }
+                "dc"    { $key = "" }
+            }
+        }
+        "2016" {
+            Switch ($namearray[2]) {
+                "std"   { $key = "" }
+                "dc"    { $key = "" }
+            }
+        }
+    }
+    
+    Write-Host -ForegroundColor Cyan "Processing $($imgfile.FullName)"
+
+    $Mount = Mount-DiskImage $imgfile.FullName -StorageType VHDX -Access ReadWrite -PassThru
+
+    $MountedDriveLetter = (Get-Disk | Where Location -eq $imgfile.FullName | Get-Partition).DriveLetter + ":"
+    
+    If (Test-Path $MountedDriveLetter) {
+
+        New-Item -Path (Split-Path ($MountedDriveLetter + "" + $UnattendPath)) -ItemType Directory -Force -Confirm:$false | Out-Null
+        New-Item -Path ($MountedDriveLetter + "" + $InstallerPath) -ItemType Directory -Force -Confirm:$false | Out-Null
+
+        $src = $ToInjectPath + "\CloudbaseInit.msi"
+        $dst = $MountedDriveLetter + "" + $InstallerPath + "\CloudbaseInit.msi"
+        Copy-Item $src $dst -Force -Confirm:$false
+
+        $src = $ToInjectPath + "\qemu-ga-x64.msi"
+        $dst = $MountedDriveLetter + "" + $InstallerPath + "\qemu-ga-x64.msi"
+        Copy-Item $src $dst -Force -Confirm:$false
+
+        $src = $UnattendSource
+        $dst = $MountedDriveLetter + "" + $UnattendPath
+        $xmlContent = (Get-Content $src) -replace "%PRODUCTKEY%",$key
+        $xmlContent | Set-Content $dst -Force -Confirm:$false
+                
+        If (Test-Path "$MountedDriveLetter\Convert-WindowsImageInfo.txt") {
+            Remove-Item "$MountedDriveLetter\Convert-WindowsImageInfo.txt" -Force -Confirm:$false | Out-Null
+        }
+    }
+
+    Dismount-DiskImage $imgfile.FullName
+    Write-Host -ForegroundColor Green "Processing $($imgfile.FullName) : OK"
+
+}
